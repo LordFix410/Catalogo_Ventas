e73640f73e273807d0345e2f452d6c7ed10be5e3
LordFix410 <abdielcarcuz@gmail.com>
2026-09-22 22:58:29 -0600

    Base del proyecto

---
+++ includes/sidebar.php
@@ -0,0 +1,131 @@
+<aside
+    class="app-sidebar sidebar-chiquis shadow"
+    data-bs-theme="dark"
+>
+    <!-- Logo -->
+    <div class="sidebar-brand">
+
+        <a href="/index.php" class="brand-link">
+
+            <img
+                src="/assets/img/AdminLTELogo.png"
+                alt="Variedades Chiquis"
+                class="brand-image opacity-75 shadow"
+            >
+
+            <span class="brand-text fw-light">
+                Variedades Chiquis
+            </span>
+
+        </a>
+
+    </div>
+
+
+    <!-- Menú -->
+    <div class="sidebar-wrapper">
+
+        <nav class="mt-2">
+
+            <ul
+                class="nav sidebar-menu flex-column"
+                data-lte-toggle="treeview"
+                data-accordion="false"
+            >
+
+                <!-- Inicio -->
+                <li class="nav-item">
+
+                    <a href="/index.php" class="nav-link">
+
+                        <i class="nav-icon bi bi-house-door"></i>
+
+                        <p>Inicio</p>
+
+                    </a>
+
+                </li>
+
+
+                <!-- Clientes -->
+                <li class="nav-item">
+
+                    <a href="/clientes/index.php" class="nav-link">
+
+                        <i class="nav-icon bi bi-people"></i>
+
+                        <p>Clientes</p>
+
+                    </a>
+
+                </li>
+
+
+                <!-- Productos -->
+                <li class="nav-item">
+
+                    <a href="/productos/index.php" class="nav-link">
+
+                        <i class="nav-icon bi bi-box-seam"></i>
+
+                        <p>Productos</p>
+
+                    </a>
+
+                </li>
+
+
+                <!-- Pedidos -->
+                <li class="nav-item">
+
+                    <a href="/pedidos/index.php" class="nav-link">
+
+                        <i class="nav-icon bi bi-cart"></i>
+
+                        <p>Pedidos</p>
+
+                    </a>
+
+                </li>
+
+
+                <!-- Seguimiento -->
+                <li class="nav-item">
+
+                    <a href="/seguimiento/index.php" class="nav-link">
+
+                        <i class="nav-icon bi bi-truck"></i>
+
+                        <p>Seguimiento</p>
+
+                    </a>
+
+                </li>
+
+
+                <!-- Separador -->
+                <li class="nav-header">
+                    CLIENTES
+                </li>
+
+
+                <!-- Portal Cliente -->
+                <li class="nav-item">
+
+                    <a href="/portal-cliente/index.php" class="nav-link">
+
+                        <i class="nav-icon bi bi-shop"></i>
+
+                        <p>Portal Cliente</p>
+
+                    </a>
+
+                </li>
+
+            </ul>
+
+        </nav>
+
+    </div>
+
+</aside>
